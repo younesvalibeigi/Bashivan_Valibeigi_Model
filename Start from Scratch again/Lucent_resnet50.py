861a38ab541c6aa948afd36e3f944029785e3a1f
Younes Valibeigi <younes.valibeigi@mail.mcgill.ca>
2023-06-12 16:18:13 -0400

    Continued on Lucent_resnet50.py, June 12, 2023

--- Start from Scratch again/Lucent_resnet50.py
+++ Start from Scratch again/Lucent_resnet50.py
@@ -34,12 +34,45 @@ for idx, (name, layer) in enumerate(resnet50.named_modules()):
 
 # render_vis works in the same way! Just substitute the appropriate layer name!
 list_of_images = render.render_vis(resnet50, "layer4_1_conv1:121", show_inline=True)
+list_of_images = render.render_vis(resnet50, "layer3_0:100", show_inline=True)
+
 
 import matplotlib.pyplot as plt
+import numpy as np
 im = list_of_images[0]
-im = im[1:]
-plt.matshow(im[:,:,:,1])
+max_im = np.zeros((128,128))
+for i in range(im.shape[3]):
+    max_im = im[0,:,:,i]
+    plt.matshow(max_im, cmap='gray')
+    plt.show()
 
 # I read up until batching
 # https://lucent.readthedocs.io/en/latest/tutorials/first_steps.html
+def show_generated_images(sequence):
+    im = sequence[0]
+    max_im = np.zeros((128,128))
+    for i in range(im.shape[3]):
+        max_im = im[0,:,:,i]
+        plt.matshow(max_im, cmap='gray')
+        plt.show()
+
+# In[]: USe the direction functino in objectives.py in optvis
+direction = torch.rand(1024, device=device)
+obj = objectives.direction(layer='layer3_0', direction=direction)
+sequence = render.render_vis(resnet50, obj, show_inline=True)
+#from lucent.misc.io.showing import animate_sequence
+show_generated_images(sequence)
+
+
+
+# In[]: Try the lucent only with the mapper
+from Mapper2 import Mapper2
+# You need to run clean_convmap.py first to run this part
+# for now Y.shape is 22 and X is 1024, 14, 14
+direction = torch.rand(22,device=device)
+obj = objectives.direction(layer='s_w', direction=direction)
+sequence = render.render_vis(mapper2, obj, show_inline=True) # <=== Does not work
+
+
+
 
